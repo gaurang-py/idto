a723818e5415e10fda70142648451d2d8cf85500
GAURANG PATEL <72779410+gaurang-py@users.noreply.github.com>
2022-06-02 19:33:30 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,9 +1,9 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot. \nMake your own bot now 👉 Source code <a href='https://github.com/GreyMatter658/auto-forward-bot'> "             
+      START = "Hii"             
 
       #About text
       ABOUT = """
-<b>📝 Language:</b> Python 3
-🥰"""
+
+"""
