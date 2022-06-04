65cdeca743387824ed1ce57cfa7e63be3b395761
GAURANG PATEL <72779410+gaurang-py@users.noreply.github.com>
2022-06-04 21:57:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
       BOT_TOKEN = "1119746739:AAGMhvpUjXI4CzIfizRC--VXilxnkJlhaf8"
       API_ID = int(12435439)
       API_HASH = "8fba0f88315f829b034f79c5cbd2c3d4"
-      CHANNEL = ['-1096425866:-1001513827997']
+      CHANNEL = ['-1001661787344:-1001513827997']
